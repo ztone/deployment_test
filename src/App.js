d275ae1756d301d6fb9c17b7c903aa05c1802b65
Stone Gislason <steinar.gislason@gmail.com>
2022-09-19 13:40:55 +0100

    menu 2 level

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ class App extends Component {
       console.log("click");
       self.setState({ isActive: !self.state.isActive });
     });
+
+    const navExpand = [].slice.call(document.querySelectorAll('.nav-expand'))
+    console.log(navExpand);
+
+    navExpand.forEach(item => {
+      //item.querySelector('.nav-expand-content').insertAdjacentHTML('afterbegin', backLink)
+      item.querySelector('.nav-link').addEventListener('click', () => item.classList.add('active'))
+      //item.querySelector('.nav-back-link').addEventListener('click', () => item.classList.remove('active'))
+    })
   }
   
   render() {
@@ -36,16 +45,23 @@ class App extends Component {
           <span>Stone Gislason</span>
         </header>
         <nav className="nav-drill">
-          <ul className="nav-items">
+          <ul className="nav-items nav-level-1">
             <li className="nav-item nav-expand">
               <a className="nav-link nav-expand-link" href="#">
-                Influencers
+                Menu
               </a>
-            </li>
-            <li className="nav-item nav-expand">
-              <a className="nav-link nav-expand-link" href="#">
-                Commissioner
-              </a>
+              <ul className="nav-items nav-expand-content">
+                <li className="nav-item nav-expand">
+                  <a className="nav-link nav-expand-link" href="#">
+                    Influencers
+                  </a>
+                </li>
+                <li className="nav-item nav-expand">
+                  <a className="nav-link nav-expand-link" href="#">
+                    Commissioner
+                  </a>
+                </li>
+              </ul>
             </li>
           </ul>
         </nav>
