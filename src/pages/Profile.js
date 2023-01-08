2333fa684e3941590143aabf5efaab9467d922e3
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 17:53:32 +0000

    my profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,19 @@ let { id } = useParams();
 return (
 <>
 <h1>My Profile {id}</h1>
-<p>This is your awesome User Profile page, yeah</p>
+<ul>
+    <li>Name: John Johnson</li>
+    <li>Town: London</li>
+    <li>Channels: <select name="channel" id="channel">
+        <option value="golf">Golf</option>
+        <option value="gym">Gym</option>
+        <option value="fitness">Fitness</option>
+        <option value="lifestyle">Lifestyle</option>
+        <option value="travel">Travel</option>
+        <option value="luxury">Luxury</option>
+        </select>
+    </li>
+</ul>
 </>
 );
 }
