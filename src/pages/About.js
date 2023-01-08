6c0187276054fc839e249f967558392639312328
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 18:02:32 +0000

    Uppercase about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-export default function UserPage() {
+export default function About() {
 let { id } = useParams();
 return (
 <>
-<h1>Hello there user {id}</h1>
-<p>This is your awesome User Profile page, yeah</p>
+<h1>About</h1>
+<p>This is your awesome User Profile page</p>
 </>
 );
 }
