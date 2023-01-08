d95a9dec34bbf512f048ac9b5a308043ddd87a89
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 15:35:52 +0000

    Routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+// We will create these two pages in a moment
+import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/:id' component={UserPage} />
+    </Routes>
     <App />
-  //</React.StrictMode>
+  </BrowserRouter>
+  //document.querySelector('#root')
 );
 
 // If you want to start measuring performance in your app, pass a function
