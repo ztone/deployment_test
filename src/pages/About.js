285643e21ba9cdb3925db63119113cf3dd0dc122
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 18:07:45 +0000

    about us

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,14 @@ export default function About() {
 let { id } = useParams();
 return (
 <>
-<h1>About</h1>
-<p>This is your awesome User Profile page</p>
+    <h1>VOI</h1>
+    <p>VOI’s leadership team brings together best in show insights and expertise from technology & informatics and specialist leisure industry sales & marketing. The hands on team have worked for clients as diverse as London’s major museum, independent UK hotel brands as well as boutique, privately owned hotels.</p>
+    <p>The team brings together proven best practices and an in-depth understanding of the commercial dynamics of hotels, venues and spas with the capabilities of progressive technology and integration</p>
+    <ul>
+        <li>Jai Hermolle	Founder & CEO</li>
+        <li>Andrew White	Commercial Director</li>
+        <li>Paul Hermolle	Development Director</li>
+    </ul>
 </>
 );
 }
