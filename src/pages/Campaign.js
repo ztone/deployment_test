e43f8169c14ba288c3655e3b7a4a05479597babe
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 17:43:00 +0000

    Campaigns

--- src/pages/Campaign.js
+++ src/pages/Campaign.js
@@ -4,8 +4,18 @@ export default function UserPage() {
 let { id } = useParams();
 return (
 <>
-<h1>Campaign {id}</h1>
-<p>This is your awesome User Profile page, yeah</p>
+    <h1>My Campaigns</h1>
+    <input type="date" id="start" name="trip-start"
+        value="2022-07-22"
+        min="2022-01-01" max="2022-12-31"></input>
+    <input type="date" id="end" name="trip-end"
+        value="2022-08-22"
+        min="2022-01-01" max="2022-12-31"></input>
+    <ul>
+        <li>Active Campaigns</li>
+        <li>Scheduled Campaigns</li>
+        <li>Completed Campaigns</li>
+    </ul>
 </>
 );
 }
