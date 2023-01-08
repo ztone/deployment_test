0fdefed1c3929507f37fa242ae911774f849aa37
Stone Gislason <stone.gislason@health.telstra.com>
2023-01-08 18:00:09 +0000

    removed code

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -14,14 +14,3 @@ return (
 </>
 );
 }
-
-// import React from 'react';
-// //import { useParams } from 'react-router-dom';
-// export default function UserPage() {
-// //let { id } = useParams();
-// return (
-// <>
-// <p>This is your awesome User Profile page</p>
-// </>
-// );
-// }
