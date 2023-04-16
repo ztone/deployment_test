b868d23261ab190da00104a25dd19519e0536892
Stone Gislason <steinar.gislason@gmail.com>
2023-04-16 13:53:10 +0100

    enquire mobile styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,12 @@ let { id } = useParams();
 return (
 <>
 <div className='container mt-3'>
-    <div className="display-1 text-center">
+    <div className="display-1 text-center mb-5">
         <img src="/images/logo.png" alt="Voice of influence" /> 
     </div>
-    <h4 className='text-center fw-bold mt-5'>About VOi</h4>
+    <div className='mid-line w-25 float-l'></div>
+    <div className='mid-line w-25 float-r'></div>
+    <div className="text-center">About VOi</div>
     <p>VOi enables creators to obtain complimentary stays at desirable venues whilst enabling them to earn their fair share of commission by offering a discount to their following through the use of promotional codes.</p>
     <p>VOi is a platform targeted at increasing hotels’ direct bookings by utilising creators’ voice of influence. VOi values the individuality of creators and their direct relationship with their audience.</p>
     <ul>
--- src/pages/Enquire.js
+++ src/pages/Enquire.js
@@ -4,11 +4,17 @@ export default function Campaigns() {
 let { id } = useParams();
 return (
 <>
-    <h1 class="text-center">Enquire</h1>
-    <p class="text-center">Thank you for enquiring to promote Feldon Valley on 9th November 2022</p>
-    <p class="text-center">Please wait for correspondence regarding the status of your Campaign – sort of thing</p>
+    <div className="display-1 text-center">
+        <img src="/images/logo.png" alt="Voice of influence" /> 
+    </div>
+    <div className='mid-line w-25 float-l'></div>
+    <div className='mid-line w-25 float-r'></div>
+    <div className="text-center">Enquire</div>
+
+    <p class="text-center mt-5">Thank you for enquiring to promote Feldon Valley on 9th November 2022</p>
+    <p class="text-center">Please wait for correspondence regarding the status of your Campaign.</p>
     <p class="text-center">
-        <Link className='btn btn-dark' to="/hotels">Back to Hotels</Link>
+        <Link className='text-center' to="/hotels">Back to Hotels</Link>
     </p>
 </>
 );
