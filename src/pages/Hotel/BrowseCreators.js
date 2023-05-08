24cbeca6dd2afdf30ee7267e7532257639ee473a
Stone Gislason <steinar.gislason@gmail.com>
2023-05-08 23:41:07 +0100

    browse creators

--- src/pages/Hotel/BrowseCreators.js
+++ src/pages/Hotel/BrowseCreators.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import TabletHeader from './TabletHeader';
 import ListMenu from './ListMenu';
+import ProfileInfo from '../Creator/ProfileInfo';
 
 export default function BrowseCreators() {
   let { search } = useParams();
@@ -22,7 +23,23 @@ return (
 
       {/* BrowseCreators */}
       <div className="col-9">
-      <div className='btn btn-dark w-75'>BrowseCreators</div><br/>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+            </div>
+            <div className='row mt-2'>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+            </div>
+            <div className='row mt-2'>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'><ProfileInfo /></div>
+            </div>
+        </div>
       </div>
     </div>
   {/* </div> */}
