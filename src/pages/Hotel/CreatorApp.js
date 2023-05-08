df58b84be306b184498d1a48d86322faba592a99
Stone Gislason <steinar.gislason@gmail.com>
2023-05-09 00:06:53 +0100

    creator app init

--- src/pages/Hotel/CreatorApp.js
+++ src/pages/Hotel/CreatorApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import TabletHeader from './TabletHeader';
 import ListMenu from './ListMenu';
+import ProfileInfo from '../Creator/ProfileInfo';
 
 export default function CreatorApp() {
   let { search } = useParams();
@@ -22,7 +23,58 @@ return (
 
       {/* CreatorApp */}
       <div className="col-9">
-      <div className='btn btn-dark w-75'>CreatorApp View</div><br/>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'>
+                    <div className="input-box w-100">
+                        <span className="prefix bg-danger">________</span>
+                        <input className='text-center' type="text" placeholder="Foodie Golf" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Engagement Rate:</span>
+                        <input className='text-center' type="text" placeholder="3%" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Conversion Rate:</span>
+                        <input className='text-center' type="text" placeholder="0.02%" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Voi Rating:</span>
+                        <input className='text-center' type="text" placeholder="4.2/5" name="username" disabled/>
+                    </div>
+                    <div className='row mt-1'>
+                        <div className='col text-center btn bg-danger text-white'>Reject</div>
+                        <div className='col text-center btn bg-success text-white'>Accept</div>
+                    </div>
+                </div>
+            </div>
+            <div className='row mt-2'>
+                <div className='col'><ProfileInfo /></div>
+                <div className='col'>
+                    <div className="input-box w-100">
+                        <span className="prefix bg-danger">________</span>
+                        <input className='text-center' type="text" placeholder="Foodie Golf" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Engagement Rate:</span>
+                        <input className='text-center' type="text" placeholder="3%" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Conversion Rate:</span>
+                        <input className='text-center' type="text" placeholder="0.02%" name="username" disabled/>
+                    </div>
+                    <div className="input-box w-100 mt-1">
+                        <span className="prefix">Voi Rating:</span>
+                        <input className='text-center' type="text" placeholder="4.2/5" name="username" disabled/>
+                    </div>
+                    <div className='row mt-1'>
+                        <div className='col text-center btn bg-danger text-white'>Reject</div>
+                        <div className='col text-center btn bg-success text-white'>Accept</div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   {/* </div> */}
