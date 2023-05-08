c5d16caeb4d5a96a5893ddc26fc3aea4fdc66281
Stone Gislason <steinar.gislason@gmail.com>
2023-05-09 00:13:03 +0100

    add search to browse

--- src/pages/Hotel/BrowseCreators.js
+++ src/pages/Hotel/BrowseCreators.js
@@ -24,7 +24,13 @@ return (
       {/* BrowseCreators */}
       <div className="col-9">
         <div className='container'>
-            <div className='row'>
+            <form action="/hotels">
+                <div className='w-100'>
+                <input className='w-75' type='text' placeholder='Search' id="search" name="search"/>
+                <input className='w-25' type="submit" value="Filter" />
+                </div>
+            </form>
+            <div className='row mt-2'>
                 <div className='col'><ProfileInfo /></div>
                 <div className='col'><ProfileInfo /></div>
                 <div className='col'><ProfileInfo /></div>
