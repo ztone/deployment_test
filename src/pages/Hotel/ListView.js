9533cfc148326085c0a205f763127682d4822569
Stone Gislason <steinar.gislason@gmail.com>
2023-05-08 23:20:45 +0100

    list view init

--- src/pages/Hotel/ListView.js
+++ src/pages/Hotel/ListView.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from "@fullcalendar/interaction"; // needed
-import listPlugin from '@fullcalendar/list'; //For List View
 import TabletHeader from './TabletHeader';
 import ListMenu from './ListMenu';
 
@@ -54,16 +50,48 @@ return (
       {/* List */}
       <div className="col-9">
         <div className="container">
-            <FullCalendar
-                plugins={[ dayGridPlugin, interactionPlugin, listPlugin ]}
-                initialView="listWeek"
-                headerToolbar={{
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'listWeek'
-                }}
-                initialEvents={INITIAL_EVENTS}
-            />
+            <div className='row'>
+                <div className='col-1 list-text'>Key</div>
+                <div className='col-1 list-text'>Campain</div>
+                <div className='col-1 list-text'>Last Edited</div>
+                <div className='col-1 list-text'>Start</div>
+                <div className='col-1 list-text'>End</div>
+                <div className='col-1 list-text'>Creators Applied</div>
+                <div className='col-1 list-text'>Creators Confirmed</div>
+                <div className='col-1 list-text'>Landing Page</div>
+                <div className='col-1 list-text'>Bookings</div>
+                <div className='col-1 list-text'>Revenue</div>
+                <div className='col-1 list-text'>Commissions</div>
+                <div className='col-1 list-text'>ROI</div>
+            </div>
+            <div className='row'>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'>Foodie Golf</div>
+                <div className='col-1 list-text'>01.05.23</div>
+                <div className='col-1 list-text'>02.05.23</div>
+                <div className='col-1 list-text'>23.05.23</div>
+                <div className='col-1 list-text'>2</div>
+                <div className='col-1 list-text'>1</div>
+                <div className='col-1 list-text'>www. feldonvalley .com/ promotions/ foodiegolf</div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+            </div>
+            <div className='row'>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'>Girls Weekend</div>
+                <div className='col-1 list-text'>01.05.23</div>
+                <div className='col-1 list-text'>24.05.23</div>
+                <div className='col-1 list-text'>24.06.23</div>
+                <div className='col-1 list-text'>1</div>
+                <div className='col-1 list-text'>1</div>
+                <div className='col-1 list-text'>www. feldonvalley. com/ promotions/ girlsweekend</div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+                <div className='col-1 list-text'></div>
+            </div>
         </div>
       </div>
     </div>
