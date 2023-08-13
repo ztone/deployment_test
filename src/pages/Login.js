b27ca1105bc414f5df453ace280b02aacd9a7bc5
Stone Gislason <steinar.gislason@gmail.com>
2023-08-13 23:33:07 +0100

    Calling server from client

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Login() {
-return (
-<>
-    <div className="display-1 text-center">
-        <img src="/images/logo.png" alt="Voice of influence" /> 
-    </div>
-    <ul className='mt-5'>
-        <p className="text-center">
-            <li> <Link className='main-menu-btn fw-bold btn btn-light btn-lg w-75' to='menu'>Creators</Link> </li>
-        </p>
-        <p className="text-center">
-            <li> <Link className='main-menu-btn fw-bold btn btn-light btn-lg w-75' to='calendar'>Hotels</Link> </li>
-        </p>
-    </ul>
-</>
+    const [apiResponse, setApiResponse] = useState('');
+
+    const fetchAPIResponse = () => {
+        fetch("http://localhost:9000/testAPI")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                setApiResponse(data)
+        })
+    }
+    
+    useEffect(() => {
+        fetchAPIResponse();
+    }, [])
+    
+    return (
+    <>
+        <p>Text:{apiResponse.name}</p>
+        <div className="display-1 text-center">
+            <img src="/images/logo.png" alt="Voice of influence" /> 
+        </div>
+        <ul className='mt-5'>
+            <p className="text-center">
+                <li> <Link className='main-menu-btn fw-bold btn btn-light btn-lg w-75' to='menu'>Creators</Link> </li>
+            </p>
+            <p className="text-center">
+                <li> <Link className='main-menu-btn fw-bold btn btn-light btn-lg w-75' to='calendar'>Hotels</Link> </li>
+            </p>
+        </ul>
+    </>
 );
 }
